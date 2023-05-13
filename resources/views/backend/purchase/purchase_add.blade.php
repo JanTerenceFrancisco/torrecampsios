28d71a2b13a5c9c45df449ebc544cf776dc1c24f
JanTerenceFrancisco <franciscoterence98@gmail.com>
2023-05-13 14:11:06 +0800

    Add Purchase Update TMIOS

--- resources/views/backend/purchase/purchase_add.blade.php
+++ resources/views/backend/purchase/purchase_add.blade.php
@@ -74,7 +74,9 @@
                                     <div class="md-3">
                                         <label for="example-text-input" class="form-label" style="margin-top:43px;"></label>
 
-                                        <i class="btn btn-secondary btn-rounded waves-effect waves-light fas fa-plus-circle addeventmore"> Add More</i>
+                                        <i
+                                            class="btn btn-secondary btn-rounded waves-effect waves-light fas fa-plus-circle addeventmore">
+                                            Add More</i>
 
                                     </div>
                                 </div>
@@ -160,7 +162,7 @@
                 <input type="number" min="1" class="form-control buying_qty text-right" name="buying_qty[]">
             </td>
             <td>
-                <input type="number" class="form-control unit_price text-right" name="unit_price[]">
+                <input type="text"  class="form-control unit_price text-right" name="unit_price[]">
             </td>
             <td>
                 <input type="text" class="form-control" name="description[]">
@@ -188,30 +190,82 @@
                 var product_name = $('#product_id').find('option:selected').text();
 
                 if (date == '') {
-                    $.notify("Date is required", {globalPosition: 'top right', className:'error' });
+                    $.notify("Date is required", {
+                        globalPosition: 'top right',
+                        className: 'error'
+                    });
                     return false;
                 }
                 if (purchase_no == '') {
-                    $.notify("Purchase Number is required", {globalPosition: 'top right', className:'error' });
+                    $.notify("Purchase Number is required", {
+                        globalPosition: 'top right',
+                        className: 'error'
+                    });
                     return false;
                 }
                 if (supplier_id == '') {
-                    $.notify("Supplier is required", {globalPosition: 'top right', className:'error' });
+                    $.notify("Supplier is required", {
+                        globalPosition: 'top right',
+                        className: 'error'
+                    });
                     return false;
                 }
                 if (category_id == '') {
-                    $.notify("Category is required", {globalPosition: 'top right', className:'error' });
+                    $.notify("Category is required", {
+                        globalPosition: 'top right',
+                        className: 'error'
+                    });
                     return false;
                 }
                 if (product_id == '') {
-                    $.notify("Product is required", {globalPosition: 'top right', className:'error' });
+                    $.notify("Product is required", {
+                        globalPosition: 'top right',
+                        className: 'error'
+                    });
                     return false;
                 }
 
-                var source = $("document-template").html();
+                var source = $("#document-template").html();
                 var template = Handlebars.compile(source);
-
+                var data = {
+                    date: date,
+                    purchase_no: purchase_no,
+                    supplier_id: supplier_id,
+                    category_id: category_id,
+                    category_name: category_name,
+                    product_id: product_id,
+                    product_name: product_name,
+                };
+                var html = template(data);
+                $("#addRow").append(html);
             });
+
+            $(document).on("click", ".removeeventmore", function(event) {
+                $(this).closest(".delete_add_more_item").remove();
+                totalAmountPrice();
+            });
+
+            $(document).on('keyup click', '.unit_price,.buying_qty', function() {
+                var unit_price = $(this).closest("tr").find("input.unit_price").val();
+                var qty = $(this).closest("tr").find("input.buying_qty").val();
+                var total = unit_price * qty;
+                $(this).closest("tr").find("input.buying_price").val(total);
+                totalAmountPrice();
+            });
+
+            // Calculate sum of amount in invoice
+
+            function totalAmountPrice() {
+                var sum = 0;
+                $(".buying_price").each(function() {
+                    var value = $(this).val();
+                    if (!isNaN(value) && value.length != 0) {
+                        sum += parseFloat(value);
+                    }
+                });
+                $('#estimated_amount').val(sum);
+            }
+
         });
     </script>
 
