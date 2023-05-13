181ea9e7139ca177a8a8733544c4c33e2ff42697
JanTerenceFrancisco <franciscoterence98@gmail.com>
2023-05-14 00:58:28 +0800

    Invoice Update TMIOS

--- database/migrations/2023_05_13_164613_create_payments_table.php
+++ database/migrations/2023_05_13_164613_create_payments_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->integer('invoice_id')->nullable();
+            $table->integer('customer_id')->nullable();
+            $table->string('paid_status',51)->nullable();
+            $table->decimal('paid_amount')->nullable();
+            $table->decimal('due_amount')->nullable();
+            $table->decimal('total_amount')->nullable();
+            $table->decimal('discount_amount')->nullable();
             $table->timestamps();
         });
     }
