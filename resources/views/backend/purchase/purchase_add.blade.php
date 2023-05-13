57b05d286a1b56e8b77620af18cf7d7ac6418cd3
JanTerenceFrancisco <franciscoterence98@gmail.com>
2023-05-13 13:40:44 +0800

    Add Purchase Update TMIOS

--- resources/views/backend/purchase/purchase_add.blade.php
+++ resources/views/backend/purchase/purchase_add.blade.php
@@ -73,8 +73,9 @@
                                 <div class="col-md-4">
                                     <div class="md-3">
                                         <label for="example-text-input" class="form-label" style="margin-top:43px;"></label>
-                                        <input type="submit" class="btn btn-secondary btn-rounded waves-effect waves-light"
-                                            value="Add More">
+
+                                        <i class="btn btn-secondary btn-rounded waves-effect waves-light fas fa-plus-circle addeventmore"> Add More</i>
+
                                     </div>
                                 </div>
                                 <!-- end col -->
@@ -137,7 +138,7 @@
         </div>
     </div>
 
-    
+
 
     <script id="document-template" type="text/x-handlebars-template">
     
@@ -173,6 +174,45 @@
     
         </tr>
         
+    </script>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $(document).on("click", ".addeventmore", function() {
+                var date = $('#date').val();
+                var purchase_no = $('#purchase_no').val();
+                var supplier_id = $('#supplier_id').val();
+                var category_id = $('#category_id').val();
+                var category_name = $('#category_id').find('option:selected').text();
+                var product_id = $('#product_id').val();
+                var product_name = $('#product_id').find('option:selected').text();
+
+                if (date == '') {
+                    $.notify("Date is required", {globalPosition: 'top right', className:'error' });
+                    return false;
+                }
+                if (purchase_no == '') {
+                    $.notify("Purchase Number is required", {globalPosition: 'top right', className:'error' });
+                    return false;
+                }
+                if (supplier_id == '') {
+                    $.notify("Supplier is required", {globalPosition: 'top right', className:'error' });
+                    return false;
+                }
+                if (category_id == '') {
+                    $.notify("Category is required", {globalPosition: 'top right', className:'error' });
+                    return false;
+                }
+                if (product_id == '') {
+                    $.notify("Product is required", {globalPosition: 'top right', className:'error' });
+                    return false;
+                }
+
+                var source = $("document-template").html();
+                var template = Handlebars.compile(source);
+
+            });
+        });
     </script>
 
     <script type="text/javascript">
