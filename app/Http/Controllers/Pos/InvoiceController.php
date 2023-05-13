b96d8ef5d7df267847817c882e3de6b451cf8a71
JanTerenceFrancisco <franciscoterence98@gmail.com>
2023-05-14 01:12:15 +0800

    Small Update Invoice TMIOS

--- app/Http/Controllers/Pos/InvoiceController.php
+++ app/Http/Controllers/Pos/InvoiceController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Pos;
 
 use App\Http\Controllers\Controller;
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
     public function InvoiceAll()
     {
-        
+        $allData = Invoice::orderBy('date', 'DESC')->orderBy('id', 'DESC')->get();
+        return view('backend.invoice.invoice_all', compact('allData'));
     }
 }
---
+++ resources/views/backend/invoice/invoice_all.blade.php
@@ -0,0 +1,71 @@
+@extends('admin.admin_master')
+@section('admin')
+    <div class="page-content">
+        <div class="container-fluid">
+
+            <!-- start page title -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                        <h4 class="mb-sm-0">Invoices</h4>
+
+
+
+                    </div>
+                </div>
+            </div>
+            <!-- end page title -->
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+
+                            <a href="{{ route('purchase.add') }}" class="btn btn-dark btn-rounded waves-effect waves-light"
+                                style="float:right;"><i class="fas fa-plus-circle"> Add Invoice</a></i> <br>
+
+                            <h4 class="card-title">Invoices Data</h4>
+
+
+                            <table id="datatable" class="table table-bordered table-striped dt-responsive nowrap"
+                                style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                <thead>
+                                    <tr>
+                                        <th>Sl</th>
+                                        <th>Customer Name</th>
+                                        <th>Invocie No</th>
+                                        <th>Date</th>
+                                        <th>Description</th>
+                                        <th>Action</th>
+                                </thead>
+
+                                <tbody>
+
+                                    @foreach ($allData as $key => $item)
+                                        <tr class="text-center">
+                                            <td> {{ $key + 1 }} </td>
+                                            <td> </td>
+                                            <td> {{ $item->invoice_no }} </td>
+                                            <td> {{ date('m/d/Y', strtotime($item->date)) }} </td>
+                                            <td> {{ $item->description }} </td>
+                                            <td>
+                                                <a href="{{ route('purchase.delete', $item->id) }}"
+                                                    class="btn btn-danger sm" title="Delete Data" id="delete"> <i
+                                                        class="fas fa-trash-alt"></i> </a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+
+                                </tbody>
+                            </table>
+
+                        </div>
+                    </div>
+                </div> <!-- end col -->
+            </div> <!-- end row -->
+
+
+
+        </div> <!-- container-fluid -->
+    </div>
+@endsection
