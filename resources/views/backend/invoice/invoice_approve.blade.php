80d4c3c7ba4916366ba125a76550892df74125c8
JanTerenceFrancisco <franciscoterence98@gmail.com>
2023-05-15 12:38:08 +0800

    Small Update TMIOS

--- resources/views/backend/invoice/invoice_approve.blade.php
+++ resources/views/backend/invoice/invoice_approve.blade.php
@@ -4,6 +4,11 @@
         <div class="container-fluid">
 
             <!-- start page title -->
+
+            @php
+                $payment = App\Models\Payment::where('invoice_id', $invoice->id)->first();
+            @endphp
+
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
@@ -21,8 +26,52 @@
                     <div class="card">
                         <div class="card-body">
 
-                            <a href="{{ route('invoice.pending.list') }}" class="btn btn-dark btn-rounded waves-effect waves-light"
-                                style="float:right;"><i class="fa fa-list"> Pending Invoices</a></i> <br>
+                            <h4>Invoice No: #{{ $invoice->invoice_no }} - {{ date('m/d/Y', strtotime($invoice->date)) }}
+                            </h4>
+
+                            <a href="{{ route('invoice.pending.list') }}"
+                                class="btn btn-dark btn-rounded waves-effect waves-light" style="float:right;"><i
+                                    class="fa fa-list"> Pending Invoices</a></i> <br>
+
+                            <br>
+                            <table class="table table-dark" width="100%">
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <p>Customer Info</p>
+                                        </td>
+                                        <td>
+                                            <p>
+                                                Customer Name: <strong>{{ $payment['customer']['customer_name'] }}</strong>
+                                            </p>
+                                        </td>
+                                        <td>
+                                            <p>Customer Phone No.:
+                                                <strong>{{ $payment['customer']['customer_phone'] }}</strong>
+                                            </p>
+                                        </td>
+                                        <td>
+                                            <p>
+                                                Customer Address: <strong>{{ $payment['customer']['customer_address1'] }},
+                                                    {{ $payment['customer']['customer_address2'] }},
+                                                    {{ $payment['customer']['customer_city'] }},
+                                                    {{ $payment['customer']['customer_province'] }},
+                                                    {{ $payment['customer']['customer_zipcode'] }}</strong>
+                                            </p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+
+                                        </td>
+                                        <td colspan="3">
+                                            <p>Description: 
+                                                <strong>{{ $invoice->description }}</strong>
+                                            </p>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
 
 
 
